eafc6f165659f3363e3286305912869249e31d22
Mahmud2640 <mdmahmud484@gmail.com>
2022-01-30 21:43:49 +0600

    first commit

---
+++ forLoop-arry.js
@@ -0,0 +1,26 @@
+var number = [12, 22, 33, 44, 56, 66, 45, 30, 10, 22, 33, 44, 55];
+for (var i = 0; i < number.length; i++) {
+   var element = number[i];
+   console.log(element);
+}
+var eat = ['ami vat kahmu', 'hello', 'ami mac kahbo', 'vat deo'];
+for (var i = 0; i < eat.length; i++) {
+   var element = eat[i];
+   console.log(element);
+   console.log(element);
+}
+
+var items = ['pen', 'paper', 'book', 'computer', 'apple']
+
+for (var i = 0; i < items.length; i++) {
+   var item = items[i];
+   console.log(item);
+}
+
+var collection = ['kasem', 'abdur rahaman', 'murad', 'sabbir', 'robiul', 'kasem', 'abdur rahaman', 'murad', 'sabbir', 'robiul']
+
+for (var i = 0; i < collection.length; i++) {
+   var element = collection[i];
+   console.log(element);
+   
+}
---
+++ while-loop.js
@@ -0,0 +1,27 @@
+// var rostGiven = 0;
+
+// while (rostGiven <= 7) {
+//    console.log('Roast den Taratrai.');
+//    rostGiven++;
+//    console.log(rostGiven);
+// }
+// var oddNumber = 0;
+// while (oddNumber <= 10) {
+//    console.log(1);
+//    oddNumber+=1;
+//    console.log(oddNumber);
+// }
+// var i = 10;
+// while (i <= 20) {
+//    console.log('hello');
+//    i++ 
+//    console.log(i);
+// }
+
+var dayWork = ['1. রাত ৮ টা বাজে মডিউল আনলক করো', '2. ফটাফট ভিডিও দেখে দেখে প্রাকটিস করো', '3.ভিডিও দেখতে দেখতে নোটস নাও', '4. মডিউল শেষ হলে পুরা মডিউল নিজে নিজে প্রাকটিস করো', '5. কোন কিছু বুঝতে না পারলে (চিন্তা করে দেখো এইখানে কিন্তু একটা শর্ত আছে ), সাপোর্ট সেশনে জয়েন করো'];
+while (i = 0) {
+   console.log(i < dayWork.length);
+   i++
+   console.log(element);
+   
+}
